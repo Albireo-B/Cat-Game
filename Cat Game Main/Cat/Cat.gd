d7a58a5cbf83d14a0a3fa290d50a6064db1c3dd0
M5-Altair <guillaume.vinet.38@gmail.com>
2020-03-30 20:41:43 +0200

    Added the cat script part for the cat animations (only left and right)

--- Cat Game Main/Cat/Cat.gd
+++ Cat Game Main/Cat/Cat.gd
@@ -3,12 +3,15 @@ extends KinematicBody2D
 signal food_changed
 signal energy_changed
 
+onready var sprite = get_node("Body")
+
 export (float) var speed
 export (float) var rotation_speed
 export (int) var energy
 export (int) var food
 
 var velocity = Vector2()
+var anim = "idle"
 var can_move = true
 #var can_eat ??
 
@@ -28,10 +31,20 @@ func control(delta):
 		velocity = Vector2(speed, 0).rotated(rotation)
 	if Input.is_action_pressed("move_backward"):
 		velocity = Vector2(-speed, 0).rotated(rotation)
-	
+	#set animation
+	if velocity.x == 0:
+		anim = "idle"
+	else :
+		if velocity.x > 0:
+			anim = "walking_right"
+		if velocity.x < 0:
+			anim = "walking_left"
+		else:
+			pass
 	
 func _physics_process(delta):
 	if not can_move:
 		return
 	control(delta)
 	move_and_slide(velocity)
+	sprite.play(anim)
