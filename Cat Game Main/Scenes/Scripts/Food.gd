7bf26c5e9fc0e02387b03481de4ac548c8b81fe0
M5-Altair <guillaume.vinet.38@gmail.com>
2020-04-02 18:53:17 +0200

    Milk bottle added and giving +30 food. Urgent project restructuration in scene  + spawner in Main

--- Cat Game Main/Scenes/Scripts/Food.gd
+++ Cat Game Main/Scenes/Scripts/Food.gd
@@ -1,2 +1,10 @@
 extends Area2D
 
+onready var cat = get_parent().get_node("../Characters/Cat")
+
+func _on_Food_body_entered(body):
+	if body.name == "Cat":
+		get_tree().queue_delete(self)
+		cat.food += 30
+		
+		
