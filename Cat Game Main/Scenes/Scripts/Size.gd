c7c8298515cfb87e7dd075e796639156da9d57da
M5-Altair <guillaume.vinet.38@gmail.com>
2020-04-04 16:25:56 +0200

    Background added and resizing almost perfect ( just GUI messing around)

--- Cat Game Main/Scenes/Scripts/Size.gd
+++ Cat Game Main/Scenes/Scripts/Size.gd
@@ -1,24 +1,23 @@
 extends Node
 
 onready var viewport = get_viewport()
-
-var minimum_size = Vector2(1920, 1080)
+onready var game_size = Vector2(
+ProjectSettings.get_setting("display/window/size/width"),
+ProjectSettings.get_setting("display/window/size/height"))
 
 func _ready():
-	viewport.connect("size_changed", self, "window_resize")
-	window_resize()
+	viewport.connect("size_changed", self, "resize_window")
+	resize_window()
 
-func window_resize():
-	var current_size = OS.get_window_size()
+func resize_window():
+	var new_size = OS.get_window_size()
+	var scale_factor
 
-	var scale_factor = minimum_size.y/current_size.y
-	var new_size = Vector2(current_size.x*scale_factor, minimum_size.y)
-
-	if new_size.y < minimum_size.y:
-		scale_factor = minimum_size.y/new_size.y
-		new_size = Vector2(new_size.x*scale_factor, minimum_size.y)
-	if new_size.x < minimum_size.x:
-		scale_factor = minimum_size.x/new_size.x
-		new_size = Vector2(minimum_size.x, new_size.y*scale_factor)
+	if new_size.x < game_size.x:
+		scale_factor = game_size.x/new_size.x
+		new_size = Vector2(new_size.x*scale_factor, new_size.y*scale_factor)
+	if new_size.y < game_size.y:
+		scale_factor = game_size.y/new_size.y
+		new_size = Vector2(new_size.x*scale_factor, new_size.y*scale_factor)
 
 	viewport.set_size_override(true, new_size)
