57a40ef61a0ac7fef6ec735c528e67aa259502cf
Rosiflette <janinejacques.tod@gmail.com>
2020-04-12 14:27:10 +0200

    Human go to the first poop

--- Cat Game Main/Scenes/Character/Human.gd
+++ Cat Game Main/Scenes/Character/Human.gd
@@ -19,35 +19,15 @@ func _ready():
 func control():
 	#set human velocity
 	velocity = Vector2()
-
 	var not_move = false
-#	                      X
-	if player.position.x > position.x and next_direction.x != 1:
-		next_direction.x = 1
-		next_direction_time = OS.get_ticks_msec() + react_time
-	elif player.position.x < position.x and next_direction.x != -1:
-		next_direction.x = -1
-		next_direction_time = OS.get_ticks_msec() + react_time
-	elif player.position.x == position.x and next_direction.x != 1:
-		next_direction.x = 1
-		next_direction_time = OS.get_ticks_msec() + react_time	
 
-#	                      Y
-	if player.position.y > position.y and next_direction.y != 1:
-		next_direction.y = 1
-		next_direction_time = OS.get_ticks_msec() + react_time
-
-	elif player.position.y < position.y and next_direction.y != -1:
-		next_direction.y = -1
-		next_direction_time = OS.get_ticks_msec() + react_time
-
-	elif player.position.y == position.y and next_direction.y != 0:
-		next_direction.y = 0
-		next_direction_time = OS.get_ticks_msec() + react_time
-	
+	if !ckeck_poops() :
+		print("a")
+		move_to(player.position)
+		
 	if next_direction.x != direction.x or next_direction.y != direction.y:
 		not_move = true
-		
+			
 	if OS.get_ticks_msec() < next_direction_time:
 		direction.x = next_direction.x
 		direction.y = next_direction.y
@@ -66,7 +46,46 @@ func control():
 		elif velocity.y < 0 :
 			anim = "walking_forward"
 
+func move_to(target):
+		#	                      X
+		if target.x > position.x:
+			set_dir(1, 0)
+		elif target.x < position.x:
+			set_dir(-1, 0)
+		elif target.x == position.x:
+			set_dir(0, 0)	
+	
+	#	                      Y
+		if target.y > position.y :
+			set_dir(1, 1)
+	
+		elif target.y < position.y:
+			set_dir(-1, 1)
+		elif target.y == position.y:
+			set_dir(0, 1)
+	
+func set_dir(orientation_dir, axis=3):
+	if axis == 0:
+		if next_direction.x != orientation_dir:
+			next_direction.x = orientation_dir
+			next_direction_time = 	OS.get_ticks_msec() + react_time
+	elif axis == 1:
+		if next_direction.y != orientation_dir:
+			next_direction.y = orientation_dir
+			next_direction_time = 	OS.get_ticks_msec() + react_time
+		
 
+func ckeck_poops() :
+	var all_poops = get_parent().get_node("Poops");
+	
+	if all_poops.get_child_count() != 0:
+		print(all_poops.get_child(0).position)
+		move_to(all_poops.get_child(0).position)
+		return true
+	else :
+		return false
+		
+	
 
 	
 func _physics_process(delta):
