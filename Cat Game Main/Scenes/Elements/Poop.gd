850f27558fc1079dd97647ad9d4b26ab35dcc1e2
M5-Altair <guillaume.vinet.38@gmail.com>
2020-04-13 15:08:58 +0200

    Human stopping 10sec on Poop ( in Poop.gd )

--- Cat Game Main/Scenes/Elements/Poop.gd
+++ Cat Game Main/Scenes/Elements/Poop.gd
@@ -2,4 +2,8 @@ extends Area2D
 
 func _on_Poop_body_entered(body):
 	if body.name == "Human":
+		var human_node = get_parent().get_parent().get_node("Human")
+		human_node.set_physics_process(false)
+		yield(get_tree().create_timer(10), "timeout")
+		human_node.set_physics_process(true)
 		queue_free()
